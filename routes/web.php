60702a776e1529b7705279d68d6f3d3734850917
Jazmin-Loureiro <jazmin.loureiro25@gmail.com>
2025-05-26 11:52:20 -0300

    Se crearn las rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/category', function () {
+    return view('category.index');
+});
+
+Route::get('/category/show/{id}', function ($id) {
+    return view('category.show', ['id' => $id]);
+});
+
+Route::get('/category/create', function () {
+    return view('category.create');
+});
+
+Route::get('/category/edit/{id}', function ($id) {
+    return view('category.edit', ['id' => $id]);
+});
+
